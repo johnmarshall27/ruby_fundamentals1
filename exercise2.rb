a62169374c955b367877c96b8666c7bcf9f25182
John Marshall <john.marshall4997@hotmail.com>
2015-06-09 14:03:57 -0400

    exercise 2 work

---
+++ exercise2.rb
@@ -0,0 +1,18 @@
+# 1.
+
+meal_price = 55
+tip_price = (meal_price*0.15)
+Total_Price = (tip_price + meal_price)
+
+puts "you tipped #{tip_price}"
+puts "your total cost is #{Total_Price}"
+
+# 3. 
+
+result = 45628*7839
+puts "the result of multiplying 45628 and 7839 is #{result}"
+
+#4. 
+
+puts (true && false) || (false && true) || !(false && false)
+
